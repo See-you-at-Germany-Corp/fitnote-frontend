1e5c975917279dd81ff0874f5edc2cad2b7ba174
Werawit <werawit_boat@windowslve.com>
2021-01-13 22:13:09 +0700

    Add: basic library setup (tailwind, antd, styled)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import { Button } from 'antd';
 
 export default function Home() {
   return (
@@ -13,6 +14,9 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+        
+        <div className="bg-red-300">Tailwind CSS Testing.</div>
+         <Button>Press Me</Button>
 
         <p className={styles.description}>
           Get started by editing{' '}
